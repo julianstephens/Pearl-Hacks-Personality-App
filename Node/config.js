965e23d76766d1e3222e0b64dd34196c24443cb9
Leah Stephens <leahstephens55@gmail.com>
2018-02-10 18:31:12 -0500

    Twitter Api Config

---
+++ Node/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'dbSAIyGFIVmCQTckXSP0QpyHk',
+  consumer_secret: '1KfzU1srmAgb1yzaNZFwfStFVczmQj1aIG8CoWwQFTrgjF9w4m',
+  access_token_key: '810846719529385984-lzuk5LHjboOmEaaeJvopS2TaUtgs6zy',
+  access_token_secret: 'KZYkYZdkbmLvUy3W2hM0PIbPrceyhlGXoBBd0n96h1YSH'
+}
