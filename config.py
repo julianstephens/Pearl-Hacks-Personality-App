a2bf1323cb7c8b6da25aa853e9b980b5619f9405
nikitabill <36337962+nikitabill@users.noreply.github.com>
2018-02-10 13:05:47 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,27 @@
 
+# Twitter Credentials
+#
+# To obtain the credentials, you must first have a Twitter account. 
+# Then, go to https://dev.twitter.com/, login, and click on "Manage Your Apps" to reach https://apps.twitter.com/.
+# Then, click "Create New App", fill in the relevant fields, and click "Create your Twitter application".
+# With the application created, navigate to the API Keys page and click "Create my access token". 
+# You now have the four necessary credentials. Copy the API key, API secret, 
+# Access token, and Access token secret here.
+# NOTE: API key and API secret go in the twitter_consumer_key and twitter_consumer_secret vars.
+#
+
+twitter_consumer_key = ''
+twitter_consumer_secret = ''
+twitter_access_token = ''
+twitter_access_secret = ''
+
+# Personality Insights credentials and URL
+#
+# You can obtain these credentials by binding a PI service to an application in bluemix and 
+# and clicking the "show credentials" link on the service in the application dashboard.
+# Or you can use "cf env <application name>" from the command line to get the credentials.
+
+pi_url = ''
+pi_username = ''
+pi_password = ''
+
