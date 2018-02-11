0b9f8b5e5824febc94f2ff4fa8ff26a89a8f7b51
Abby Bilenkin <aabilenkin@email.wm.edu>
2018-02-10 20:00:41 -0500

    removed code that's no longer in use

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,15 +2,12 @@
 
 import { HomeComponent } from './home/index';
 import { LoginComponent } from './login/index';
-import { RegisterComponent } from './register/index';
-import { AuthGuard } from './_guards/index';
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
+    { path: '', component: LoginComponent },
+    { path: '', component: HomeComponent },
 
-    // otherwise redirect to home
+    // otherwise redirect to login
     { path: '**', redirectTo: '' }
 ];
 
